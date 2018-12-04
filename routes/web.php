5c780dc254f6efd7353c946ed7728b4d6d44a67c
DungLQ159 <quangdunglu159@gmail.com>
2018-12-04 20:31:08 +0700

    - Add route

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,58 @@ Route::get("khoahoc", function () {
 Route::get("khoapham/laravel", function () {
     echo "<h1>Xin chao laravel</h1>";
 });
+
+// Truyền tham số cho route
+// http://localhost:8090/DungLQ7_Laravel1/public/hoten/dung
+Route::get("hoten/{ten}", function ($ten) {
+    echo $ten;
+});
+
+Route::get("ten/{ten?}", function ($ten = "default") {
+    echo $ten;
+});
+
+// http://localhost:8090/DungLQ7_Laravel1/public/laravel/20
+Route::get("laravel/{ngay}", function ($ngay) {
+    echo $ngay;
+})->where(["ngay" => "[0-9]+"]);
+
+// Định danh cho route
+Route::get("route1", ["as" => "myRoute", function () {
+    echo "Xin chào mấy bố";
+}]);
+
+// Định danh route : cách 2
+Route::get("route2", function () {
+    echo "route 2";
+})->name("myRoute2");
+
+// Gọi tên route đã được đặt tên
+// http://localhost:8090/DungLQ7_Laravel1/public/goiten
+Route::get("goiten", function () {
+    return redirect()->route("myRoute");
+});
+
+// http://localhost:8090/DungLQ7_Laravel1/public/goiten2
+Route::get("goiten2", function () {
+    return redirect()->route("myRoute2");
+});
+
+// Nhóm route
+Route::group(["prefix" => "myGroup"], function () {
+
+    // http://localhost:8090/DungLQ7_Laravel1/public/myGroup/user1
+    Route::get("user1", function () {
+        echo "user 1";
+    });
+
+    // http://localhost:8090/DungLQ7_Laravel1/public/myGroup/user2
+    Route::get("user2", function () {
+        echo "user 2";
+    });
+
+    // http://localhost:8090/DungLQ7_Laravel1/public/myGroup/user3
+    Route::get("user3", function () {
+        echo "user 3";
+    });
+});
